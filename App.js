2c96c13c8a154e702bceb518e3f7862ae8b488d2
Sherange <SFonseka@tandemdiabetes.com>
2020-04-19 21:26:50 +0530

    react native splash screen

--- App.js
+++ App.js
@@ -6,14 +6,19 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, StatusBar, SafeAreaView} from 'react-native';
 import Home from './screens/Home';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({duration: 250});
+  }, []);
+
   return (
     <React.Fragment>
-      <StatusBar barStyle="light-content" backgroundColor={'#OOOOOO'} />
+      <StatusBar barStyle="light-content" backgroundColor={'#000000'} />
       <SafeAreaView style={styles.safeAreaView} />
       <Home />
     </React.Fragment>
